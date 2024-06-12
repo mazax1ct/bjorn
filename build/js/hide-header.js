7ae45be8f4739935ad37ffcfe7037cbdcbc07e81
mazax1ct <vova@giveandget.ru>
2024-06-12 17:40:50 +0300

    catalog-section + search-result

---
+++ build/js/hide-header.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+  var prevScrollpos = window.pageYOffset;
+  var header = document.querySelector('.header');
+  var headerHeight = header.offsetHeight;
+  var catalogTop = document.querySelector('.catalog-top');
+  var catalogTopHeight = catalogTop.offsetHeight;
+  var catalogTopOffsetTop = catalogTop.offsetTop;
+  var catalogTopInner = document.querySelector('.catalog-top__inner');
+  var catalogTopMenu = document.querySelector('.catalog-top__menu');
+  var catalogMenu = document.querySelector('.catalog-menu');
+
+  window.onscroll = function() {
+    var currentScrollPos = window.pageYOffset;
+
+    if(currentScrollPos >= (catalogTopOffsetTop - headerHeight)) {
+      catalogMenu.classList.add('is-fixed');
+      catalogMenu.style.top = headerHeight + 'px';
+      catalogTopMenu.classList.add('menu-is-fixed');
+    } else {
+      catalogMenu.classList.remove('is-fixed');
+      catalogTopMenu.classList.remove('menu-is-fixed');
+    }
+
+    if (prevScrollpos > currentScrollPos) {
+      header.classList.remove('is-out');
+      catalogTop.classList.add('is-out');
+    } else {
+      header.classList.add('is-out');
+      catalogTop.classList.remove('is-out');
+    }
+
+    prevScrollpos = currentScrollPos;
+  }
+});
