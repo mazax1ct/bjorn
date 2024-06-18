11771a8148b9c2d2c6f8d53e57430da9b0512ede
mazax1ct <vova@giveandget.ru>
2024-06-18 21:26:43 +0300

    gift-card

---
+++ src/js/gift-card.js
@@ -0,0 +1,26 @@
+const thumbs = new Swiper(".js-gift-card-thumbs", {
+  loop: true,
+  spaceBetween: 6,
+  slidesPerView: 'auto',
+  freeMode: true,
+  watchSlidesProgress: true,
+  breakpoints: {
+    1024: {
+      slidesPerView: 5
+    },
+    1600: {
+      slidesPerView: 5,
+      spaceBetween: 12
+    }
+
+
+  }
+});
+
+const slider = new Swiper(".js-gift-card-image", {
+  loop: true,
+  spaceBetween: 8,
+  thumbs: {
+    swiper: thumbs
+  },
+});
