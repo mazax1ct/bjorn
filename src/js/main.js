a4592644e10c5776ffc2b00939eb30d5b65fe112
mazax1ct <vova@giveandget.ru>
2024-06-27 17:47:37 +0300

    main

--- src/js/main.js
+++ src/js/main.js
@@ -49,4 +49,56 @@ document.addEventListener('DOMContentLoaded', function() {
       }
     });
   });
+
+  if(window.innerWidth >= 1300) {
+    let slides = document.querySelectorAll('.heroes-slider__slide');
+    let prev = document.querySelector('.heroes-slider__prev');
+    let next = document.querySelector('.heroes-slider__next');
+    let currIndex = 0;
+
+    slides[currIndex].classList.add('is-active');
+    slides[currIndex + 1].classList.add('is-active');
+
+    prev.addEventListener('click', function (event) {
+      if(currIndex - 2 >= 0) {
+        slides[currIndex].classList.remove('is-active');
+        slides[currIndex + 1].classList.remove('is-active');
+
+        slides[currIndex - 1].classList.add('is-active');
+        slides[currIndex - 2].classList.add('is-active');
+
+        currIndex = currIndex - 2;
+      } else {
+        slides[currIndex].classList.remove('is-active');
+        slides[currIndex + 1].classList.remove('is-active');
+
+
+        slides[slides.length - 1].classList.add('is-active');
+        slides[slides.length - 2].classList.add('is-active');
+
+        currIndex = slides.length - 2;
+
+      }
+    });
+
+    next.addEventListener('click', function (event) {
+      if(currIndex + 2 < slides.length) {
+        slides[currIndex].classList.remove('is-active');
+        slides[currIndex + 1].classList.remove('is-active');
+
+        slides[currIndex + 2].classList.add('is-active');
+        slides[currIndex + 3].classList.add('is-active');
+
+        currIndex = currIndex + 2;
+      } else {
+        slides[currIndex].classList.remove('is-active');
+        slides[currIndex + 1].classList.remove('is-active');
+
+        currIndex = 0;
+
+        slides[currIndex].classList.add('is-active');
+        slides[currIndex + 1].classList.add('is-active');
+      }
+    });
+  }
 });
