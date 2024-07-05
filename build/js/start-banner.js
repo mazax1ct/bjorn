0362899ac551c8efb84594408248e97a6724c126
mazax1ct <vova@giveandget.ru>
2024-07-05 12:10:43 +0300

    refaktor start-banner.js

--- build/js/start-banner.js
+++ build/js/start-banner.js
@@ -10,13 +10,27 @@ document.addEventListener('DOMContentLoaded', function() {
     var currentScrollPos = window.pageYOffset;
 
     if(currentScrollPos >= (startBannerHeight - headerHeight)) {
-      header.classList.add('header--color-transition');
+      header.classList.add('header--no_color_transition');
+
       setTimeout(() => {
         header.classList.add('header--white');
-      }, 1);
+        header.classList.remove('header--transparent');
+      }, 10);
+
+      setTimeout(() => {
+        header.classList.remove('header--no_color_transition');
+      }, 10);
     } else {
-      header.classList.remove('header--color-transition');
-      header.classList.remove('header--white');
+      header.classList.add('header--no_color_transition');
+
+      setTimeout(() => {
+        header.classList.remove('header--white');
+        header.classList.add('header--transparent');
+      }, 10);
+
+      setTimeout(() => {
+        header.classList.remove('header--no_color_transition');
+      }, 10);
     }
 
     prevScrollpos = currentScrollPos;
