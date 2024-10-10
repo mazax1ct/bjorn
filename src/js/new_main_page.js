26304764905f15f87dd9cac000979a47194ce15a
mazax1ct <vova@giveandget.ru>
2024-10-10 19:03:49 +0300

    vertical + horizontal slider

--- src/js/new_main_page.js
+++ src/js/new_main_page.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
       direction: 'vertical',
       loop: false,
       pagination: {
-        el: '.swiper-pagination'
+        el: '.js-vms-pagination'
       },
       navigation: {
         nextEl: '.js-vertical-mobile-slider-next',
@@ -13,9 +13,73 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }
 
-  var swiper2 = new Swiper(".mySwiper2", {
+  let horizontalSliders = document.querySelectorAll('.js-horizontal-slider');
+
+  horizontalSliders.forEach((slider, index) => {
+    let sliderInst = new Swiper(slider, {
+      loop: true,
+      autoplay: {
+         delay: 5000,
+       },
+       speed: 400,
       pagination: {
-        clickable: true,
-      }
+        el: '.swiper-pagination[data-pager="'+slider.dataset.pager+'"]',
+          clickable: true,
+        },
+        navigation: {
+          nextEl: '.swiper-button-next[data-arrows="'+slider.dataset.arrows+'"]',
+          prevEl: '.swiper-button-prev[data-arrows="'+slider.dataset.arrows+'"]',
+        },
+        breakpoints: {
+          1280: {
+            speed: 400
+          }
+        },
+        on: {
+          afterInit: function () {
+            setTimeout(() => {
+              let video = slider.querySelectorAll(".swiper-slide-active video");
+
+              if(video.length) {
+                sliderInst.autoplay.stop();
+                video[0].play();
+                video[0].addEventListener('ended', () => {
+                  setTimeout(() => {
+                    video[0].currentTime = 0;
+                    sliderInst.slideNext();
+                    sliderInst.autoplay.start();
+                  }, 500);
+                });
+              }
+            }, 100);
+          },
+
+          slideChangeTransitionStart: function () {
+            let videos = slider.querySelectorAll(".swiper-slide video");
+            videos.forEach((video) => {
+              video.pause();
+              video.currentTime = 0;
+            });
+          },
+
+          slideChangeTransitionEnd: function () {
+            setTimeout(() => {
+              let video = slider.querySelectorAll(".swiper-slide-active video");
+
+              if(video.length) {
+                sliderInst.autoplay.stop();
+                video[0].play();
+                video[0].addEventListener('ended', () => {
+                  setTimeout(() => {
+                    video[0].currentTime = 0;
+                    sliderInst.slideNext();
+                    sliderInst.autoplay.start();
+                  }, 500);
+                });
+              }
+            }, 100);
+          }
+        },
     });
+  })
 });
